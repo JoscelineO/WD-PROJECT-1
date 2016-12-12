56e3b0a56a6dae0014fb6db58ff813646d4a344a
JoscelineO <josceline@mac.com>
2016-12-12 16:22:21 +0000

    working timing and score-counter

--- js/app.js
+++ js/app.js
@@ -1,53 +1,18 @@
-$(start);
+var Game = Game || {};
 
-function start(){
-  makeBackground();
-  // chooseBackgroundColour();
-
-}
-
-var counter = 0;
-
-var backgroundColours = ['#FFC300', '#FF5733', '#C70039', '#900C3F', '#581845'];
-// var rand = Math.floor(Math.random()*(backgroundColours.length));
-
-var docWidth = $(document).width().parseFloat();
-
-function makeBackground() {
-  var $background = $('<div/>').css({
-    'display': 'block',
-    'width': docWidth + 'px',
-    'height': '700px',
-    'margin': '0',
-    'background-color': '#FFC300'
-  }).attr('class', 'background');
-
-  $('body').append($background).hide().fadeIn(500);
-
-  setTimeout(function() {
-    $($background).fadeOut(2000);
-  }, 500);
-
-  for (var i = 0; i < 20; i++) {
-    makeRandomSpots(i);
-  }
-
-}
-// function chooseBackgroundColour(){
-
-// vary spots colour
-function makeRandomSpots(i){
+Game.makeRandomSpots = function(i) {
   var colorOfSpot;
+
   if (i <= 4){
-    colorOfSpot = backgroundColours[0];
+    colorOfSpot = this.backgroundColours[0];
   } else if(i <= 8){
-    colorOfSpot = backgroundColours[1];
+    colorOfSpot = this.backgroundColours[1];
   } else if(i <= 12){
-    colorOfSpot = backgroundColours[2];
+    colorOfSpot = this.backgroundColours[2];
   } else if(i <= 16){
-    colorOfSpot = backgroundColours[3];
+    colorOfSpot = this.backgroundColours[3];
   } else if(i <= 20){
-    colorOfSpot = backgroundColours[4];
+    colorOfSpot = this.backgroundColours[4];
   }
   // vary spots size
   var spotSize = ((Math.random()*100) + 50).toFixed();
@@ -56,6 +21,7 @@ function makeRandomSpots(i){
   var posx = (Math.random() * ($(document).width() - spotSize)).toFixed();
   var posy = (Math.random() * ($(document).height() - spotSize)).toFixed();
 
+  //make spot divs
   var $randomSpots = $('<div/>').css({
     'width': spotSize +'px',
     'height': spotSize +'px',
@@ -64,21 +30,45 @@ function makeRandomSpots(i){
     'top': posy +'px'
   }).attr('class', 'randomSpots');
 
-  $('body').append($randomSpots).hide().fadeIn(500);
+  $('main').append($randomSpots).hide().fadeIn(1000);
+
+  $($randomSpots).on('click', function() {
+
+    if ($(this).css('background-color') === Game.randomColor){
+      Game.score++;
+      $('#counter span').html('' + Game.score);
+    }
+    $(this).remove();
+  });
 
   setTimeout(function() {
     $($randomSpots).fadeOut(5000);
-  }, 500);
+  }, 1000);
+};
 
-  $($randomSpots).on('click', function() {
-    $('#counter').html(counter);
-    counter++;
-    $(this).remove();
+Game.showSpots = function() {
+  for (var i = 0; i < 20; i++) {
+    this.makeRandomSpots(i);
+  }
+};
 
-    console.log(counter);
-  });
-}
+Game.giveBackgroundColor = function() {
+  var self = this;
+  this.randomColor = this.backgroundColours[Math.floor(Math.random()*this.backgroundColours.length)];
+  $('main').css('background', this.randomColor);
 
-// https://api.jquery.com/event.stoppropagation/
+  setTimeout(function() {
+    $('main').css('background', '#fff');
+    self.showSpots();
+  }, 2000
+);
+};
 
-//   $(.background).animate({backgroundColor: backgroundColours[rand]}, '1000');
+
+Game.setup = function() {
+  this.backgroundColours = ['rgb(46, 66, 114)', 'rgb(50, 138, 46)', 'rgb(170, 57, 57)', 'rgb(89, 42, 113)', 'rgb(225, 220, 40)'];
+  this.score = 0;
+  this.giveBackgroundColor();
+};
+
+$(Game.setup.bind(Game));
